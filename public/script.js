703eaa62e7ce7177ecf676dc92bfb50693baea64
Readri205 <read205@outlook.com>
2021-02-24 13:09:53 +0000

    amend server syntax

--- public/script.js
+++ public/script.js
@@ -39,10 +39,10 @@ function connectToNewUser(userId, stream) {
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
-  })
+})
   call.on('close', () => {
     video.remove()
-  })
+})
 
   peers[userId] = call
 }
--- server.js
+++ server.js
@@ -25,4 +25,5 @@ io.on('connection', socket => {
     })
   })
 })
+// server.listen(3000)
 server.listen(process.env.PORT || 5000)
